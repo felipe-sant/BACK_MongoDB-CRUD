a8d595f516b81837b9c6ce5403dc47b2cc226061
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-09-24 14:35:11 -0300

    feat: adicionando conexao com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import connectToDatabase from "./connection/mongoDB";
+
+async function main() {
+    try {
+        const db = await connectToDatabase()
+        const collection = db.collection("cliente")
+
+        const result = await collection.insertOne({ nome: "Pedro Augusto" })
+        console.log("Documento inserido com sucesso!", result)
+
+        const documentos = await collection.find({}).toArray()
+        console.log("Documentos encontrados:", documentos)
+    } catch (error) {
+        console.error("Erro:", error)
+    }
+}
+
+main()
